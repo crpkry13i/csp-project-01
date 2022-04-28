8cbcabf46fa98b7aae4357ce65eea9b45c86f5b1
crpkry13i <ryleinathaniel@gmail.com>
2022-04-28 15:31:35 -0500

    added code for heroku to connect to postgres db

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql:///users_db')
