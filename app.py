c29d7bc081795592db014a117fee6a7ccc28b981
crpkry13i <ryleinathaniel@gmail.com>
2022-04-28 16:20:41 -0500

    added code to connect to Heroku Postgres Services

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
+engine = create_engine(uri, echo=True)
+
 
 app = Flask(__name__)
 
