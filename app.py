84ba94b63c1f4289c654fc59bc08c26735d01492
crpkry13i <ryleinathaniel@gmail.com>
2022-04-28 16:07:32 -0500

    added code to connect to Heroku Postgres Services

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import re
 from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-# from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError
 import tekore as tk
 import asyncio
 from models import db, connect_db, User
@@ -12,10 +12,6 @@ from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
-uri = os.getenv('DATABASE_URL')  # or other relevant config var
-if uri and uri.startswith('postgres://'):
-    uri = uri.replace('postgres://', 'postgresql://', 1)
-# rest of connection code using the connection string `uri`
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql:///users_db')
@@ -24,6 +20,11 @@ app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secretbackup')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 
 
 BASE_API = "https://api.spotify.com"
@@ -67,21 +68,25 @@ def register():
     """
 
     form = RegisterForm()
-
     if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
+        try:
+            username = form.username.data
+            password = form.password.data
 
-        user = User.register(username, password)
-        db.session.add(user)
-        db.session.commit()
+            user = User.register(username, password)
+            db.session.add(user)
+            db.session.commit()
 
-        session["user_id"] = user.id
+            session["user_id"] = user.id
+
+        except IntegrityError:
+            flash("Username already taken")
+            return render_template("register.html", form=form)
 
         flash(f"Welcome, {user.username}!")
+
         return redirect("/ryleinathaniel")
     else:
-        flash("Username already taken, please try again.")
         return render_template("register.html", form=form)
 
 
